86dafeb3bc5157f6ac288a9f7aee698e7561d184
ghgmdr <truongtq20@gmail.com>
2023-03-14 08:55:21 +0700

    up

--- s11_AJAX/exercise/blog_optional/src/main/java/com/example/blog_optional/repository/ICategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.blog_optional.repository;
-
-import com.example.blog_optional.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICategoryRepository extends JpaRepository<Category,Integer> {
-}
