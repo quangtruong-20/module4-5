e9f647c9fd7d3d7254154f1714da94eb00c63a26
ghgmdr <truongtq20@gmail.com>
2023-03-29 13:25:17 +0700

    up

---
+++ module-5/ss1_es6/course.js
@@ -0,0 +1,61 @@
+
+let courses = [
+    {
+        id: 1,
+        title: "ReactJS Tutorial",
+        rating: 4.2,
+    },
+    {
+        id: 2,
+        title: "Angular Tutorial",
+        rating: 2.5,
+    },
+    {
+        id: 3,
+        title: "VueJS Tutorial",
+        rating: 3.8,
+    },
+    {
+        id: 4,
+        title: "Java Tutorial",
+        rating: 4,
+    },
+    {
+        id: 5,
+        title: "JavaScript Tutorial",
+        rating: 3.5,
+    },
+];
+
+    let addedCourses = [
+    {
+        id: 6,
+        title: "PHP Tutorial",
+        rating: 3,
+    },
+    {
+        id: 7,
+        title: "C# Tutorial",
+        rating: 2,
+    },
+    {
+        id: 8,
+        title: "Docker Tutorial",
+        rating: 3.8,
+    }
+];
+
+// Yêu cầu 1: Sử dụng cú pháp ES6 để xuất ra màn hình danh sách các bài đăng có rating ≥ 4
+let result = courses.filter( courses => courses.rating >= 4)
+console.log(result);
+
+// Yêu cầu 2: Sử dụng cú pháp ES6 để xuất ra màn hình danh sách các bài đăng có rating < 4 với yêu cầu giá trị các phần tử của mảng mới có định dạng:
+// ví dụ: [“2 - Angular Tutorial - 2.5”]
+let result2 = addedCourses.filter(addedCourse => addedCourse.rating < 4)
+let result3 = result2.map(addedCourse => addedCourse.id + " - " + addedCourse.title + " - " + addedCourse.rating)
+console.log(result3);
+
+
+//Yêu cầu 3: Sử dụng cú pháp ES6 đã học viết hàm trả về một mảng mới gộp 2 mảng courses và addedCourses
+const sum = [...courses,...addedCourses];
+console.log(sum);
