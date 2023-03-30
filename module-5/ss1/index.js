66291ab2ee542de3d21d61b0221acebcdbc9d206
ghgmdr <truongtq20@gmail.com>
2023-03-30 14:43:02 +0700

    update git

--- module-5/ss1/index.js
+++
@@ -1,3 +0,0 @@
-let x = [1,2,3,4];
-let y =  ;
-console.log(x);
---
+++ module-5/ss1_es6/test.js
@@ -0,0 +1,14 @@
+// let colors = ['red','green','blue'];
+// for (const [index,color] of colors.entries()) {
+//     console.log(`${color} is at index ${index}`);
+// }
+
+// let color = new Map();
+// color.set('adf','gfd');
+// color.set('cvbc','gerg');
+// color.set('hgkf','mbnb');
+// for (const colorElement of color) {
+//     console.log(colorElement);
+// }
+
+//class
