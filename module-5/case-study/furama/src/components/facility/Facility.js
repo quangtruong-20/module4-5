8978ef4239589cb7564f78b34a48ec733381ad23
ghgmdr <truongtq20@gmail.com>
2023-04-04 20:19:13 +0700

    nop bai

---
+++ module-5/case-study/furama/src/components/facility/Facility.js
@@ -0,0 +1,23 @@
+const facilityList = [
+    {
+        img: "https://furamavietnam.com/wp-content/uploads/2018/03/Vietnam_Danang_Furama_Ocean-Suite-Feature-768x497.jpg",
+        name: "PHÒNG SUITE HƯỚNG BIỂN",
+        area: "Diện tích phòng: 85.8 m2",
+        price: "20000",
+    },
+    {
+        img: "https://furamavietnam.com/wp-content/uploads/2018/08/Vietnam_Danang_Furama_Villas_Pool_Villas-F-768x497.jpg",
+        name: "PHÒNG DELUXE HƯỚNG BIỂN",
+        area: "Diện tích phòng: 40.1 m2",
+        price: "20000",
+    },
+    {
+        img: "https://furamavietnam.com/wp-content/uploads/2018/08/Vietnam_Danang_Furama_Villas_Beach_Pool_Villas-_Exterior-1-F-768x497.jpg",
+        name: "PHÒNG SUPERIOR HƯỚNG HỒ",
+        area: "Diện tích phòng: 43.7 m2",
+        price: "20000",
+    },
+
+];
+
+export default facilityList;
