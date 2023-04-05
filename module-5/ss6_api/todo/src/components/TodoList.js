cd686c3b1237b7cb9f5a461189967bf2fca8189e
ghgmdr <truongtq20@gmail.com>
2023-04-05 19:36:09 +0700

    nop bai

--- module-5/ss6_api/todo/src/components/TodoList.js
+++ module-5/ss6_api/todo/src/components/TodoList.js
@@ -3,50 +3,50 @@ import axios from "axios";
 import {Field, Form, Formik} from "formik";
 
 export default function TodoList() {
-        const [list,setList] = useState([]);
-        useEffect(()=> {
+    const [list, setList] = useState([]);
+    useEffect(() => {
         const fetchApi = async () => {
-          try  {
-              const result = await axios.get('https://jsonplaceholder.typicode.com/todos');
+            try {
+                const result = await axios.get('https://jsonplaceholder.typicode.com/todos');
                 setList(result.data);
-          }catch (err){
-              console.log(err);
-          }
+            } catch (err) {
+                console.log(err);
+            }
         }
         fetchApi();
 
 
-        },[]);
+    }, []);
 
-    return(
+    return (
 
         <>
-    <Formik initialValues={{title:''}} onSubmit={(values)=> {
-        const create = async () => {
-            try {
-                const result = await axios.post('https://jsonplaceholder.typicode.com/todos',{title: values.title});
-                setList([...list,result.data]);
-                alert("tạo thành công")
-            } catch (err) {
-                console.log(err);
+            <Formik initialValues={{title: ''}} onSubmit={(values) => {
+                const create = async () => {
+                    try {
+                        const result = await axios.post('https://jsonplaceholder.typicode.com/todos', {title: values.title});
+                        setList([...list, result.data]);
+                        alert("tạo thành công")
+                    } catch (err) {
+                        console.log(err);
+                    }
+                };
+
+                create();
+
             }
-        };
+            }>
+                <Form>
 
-        create();
+                    <h1>Todo List</h1>
+                    <Field name={'title'} type="text"/>
+                    <button type={'submit'}>Submit</button>
+                </Form>
 
-    }
-    }>
-        <Form>
-
-            <h1>Todo List</h1>
-            <Field name={'title'} type="text"/>
-            <button type={'submit'}>Submit</button>
-        </Form>
-
-    </Formik>
+            </Formik>
 
             {
-                list.map((value,index) =>(
+                list.map((value, index) => (
                     <tr key={index}>
                         <td>{value.title}</td>
                     </tr>
