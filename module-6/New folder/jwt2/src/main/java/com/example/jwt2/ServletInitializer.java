d917bba12c7b1c4a765e8d4c814c3bf7bf94d78b
ghgmdr <truongtq20@gmail.com>
2023-04-23 21:25:24 +0700

    up

---
+++ module-6/New folder/jwt2/src/main/java/com/example/jwt2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.jwt2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Jwt2Application.class);
+	}
+
+}
---
+++ module-6/New folder/jwt2/src/main/java/com/example/jwt2/security/SecurityInit.java
@@ -0,0 +1,9 @@
+package com.example.jwt2.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+
+
+    // đăng ký filter chain là bộ lọc các http request toới controller
+}
